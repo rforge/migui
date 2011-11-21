18d193903ed8d2a4372dc3198d3624e7a8d09f38
yusung <yusung@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-21 12:20:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/migui@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/load.first.R
+++ pkg/R/load.first.R
@@ -2,12 +2,13 @@
   mylib <- dirname(system.file(package = "migui"))
   ver <- packageDescription("migui", lib = mylib)$Version
   builddate <- packageDescription("migui", lib = mylib)$Date
-  cat(paste("\nmigui (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
-  if(!any(search()=="package:tcltk"))
-    require(tcltk)
-  if(!any(search()=="package:tkrplot"))
-    require(tkrplot)
-  if(!any(search()=="package:mi"))
-    require(mi)
-  #migui()
+  #cat(paste("\nmigui (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
+  packageStartupMessage(paste("\nmigui (Version ", ver, ", built: ", builddate, ")\n", sep = ""))
+#  if(!any(search()=="package:tcltk"))
+#    require(tcltk)
+#  if(!any(search()=="package:tkrplot"))
+#    require(tkrplot)
+#  if(!any(search()=="package:mi"))
+#    require(mi)
+#  #migui()
 }
